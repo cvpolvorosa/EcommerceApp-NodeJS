c7b177b1109c0a840dee602c58538964a0f50b53
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-04 22:58:30 +0800

    Refactored

--- app.js
+++ app.js
@@ -5,20 +5,13 @@ const bodyParser = require('body-parser'); //npm install --save body-parser
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:true})); //Parses encoded data from the form then proceeds to next()
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.use('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 const server = http.createServer(app);
 
