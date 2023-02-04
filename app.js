acfd4110c012f2c89bc1fce277991cea4bf139a7
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-04 23:08:20 +0800

    Added a 404 page handler

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended:true})); //Parses encoded data from the form then proceeds to next()
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes); //adds /admin as the starting path for adminRoutes; also don't need to declare /admin everytime
 app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
 
 const server = http.createServer(app);
 
