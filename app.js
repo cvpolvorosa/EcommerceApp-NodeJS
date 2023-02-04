1fe4f03e1b6ffb74aa47d8703d0aec38cddecce4
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-05 00:26:09 +0800

    Added styling to html files

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({extended:true})); //Parses encoded data from the form then proceeds to next()
+app.use(bodyParser.urlencoded({ extended: true })); //Parses encoded data from the form then proceeds to next()
 
-app.use('/admin',adminRoutes); //adds /admin as the starting path for adminRoutes; also don't need to declare /admin everytime
+app.use('/admin', adminRoutes); //adds /admin as the starting path for adminRoutes; also don't need to declare /admin everytime
 app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname,'views', '404.html'))
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 const server = http.createServer(app);
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.sendFile(path.join(__dirname,'../','views', 'shop.html')); //__dirname is a global var that holds the absolute path on the OS to the project folder
+router.get('/', (req, res, next) => {
+    //res.sendFile(path.join(__dirname,'../','views', 'shop.html')); //__dirname is a global var that holds the absolute path on the OS to the project folder
     //used ../ to go up one level because shop.js file is under routes folder
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
